8059c125d9f3bba6c82b47699032843d5736b5d2
Lebogang Komape <marvenkom272@gmail.com>
2023-07-31 16:36:44 +0200

    first commit practice

---
+++ main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <iomanip>
+using namespace std;
+
+int main()
+{
+
+  int i=0;
+
+  do {
+    cout<<i<<endl;
+
+    i = i + 5;
+  }
+  while(i<=20);
+
+    return 0;
+}
